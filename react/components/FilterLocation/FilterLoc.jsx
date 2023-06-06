b14fa0dff3aae0b052a5dcc29e2b7398660b30a1
kadirretir <kobraninkilici@gmail.com>
2023-06-06 22:59:35 +0300

    react adjustments

--- react/components/FilterLocation/FilterLoc.jsx
+++ react/components/FilterLocation/FilterLoc.jsx
@@ -1,46 +1,53 @@
-import React, {useState} from "react"
+import React, {useState, useEffect, useRef } from "react"
 import styles from './filterLocation.module.css'
+import itemList from './cityData.js'
 
+const FilterLocation = () => {
+    
+      const [filteredList, setFilteredList] = useState(itemList);
+      const inputElement = useRef();
 
-const FilterLocation = (props) => {
-  console.log(props)
-    const itemList = [
-        "İstanbul",
-        "Ankara",
-        "İzmir",
-        "Cherry",
-        "Milk",
-        "Peanuts",
-        "Butter",
-        "Tomato"
-      ];
-    
-      const [filteredList, setFilteredList] = new useState(itemList);
-    
       const filterBySearch = (event) => {
-        // Access input value
-        const query = event.target.value;
-        // Create copy of item list
-        var updatedList = [...itemList];
-        // Include all elements which includes the search query
-        updatedList = updatedList.filter((item) => {
-          return item.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+        const query = event.target.value.toLowerCase(); // Arama sorgusunu küçük harflere dönüştür
+        
+        let updatedList = itemList.filter((item) => {
+          return item.sehir.toLowerCase().includes(query); // Eşleşmeyi includes() yöntemiyle kontrol et
         });
-        // Trigger render with updated values
+        
+        if (updatedList.length >= 8) {
+          updatedList = []; // Eğer güncellenmiş liste 8 veya daha fazla öğe içeriyorsa, listeyi boşalt
+        }
+        
         setFilteredList(updatedList);
       };
+
+      const takeClickedCity = (item, e) => {
+        e.preventDefault();
+        inputElement.current.value = item
+        setFilteredList([])
+      }
+
+
+      useEffect(() => {
+        setFilteredList([])
+      }, [])
       return (
         <div className={styles.App}>
           <div className={styles.searchHeader}>
-            <input id={styles.searchBox} className="form-control" onChange={filterBySearch} />
+            <input id={styles.searchBox} ref={inputElement} className="form-control my-2" placeholder="İstanbul, Ankara, İzmir..." onChange={filterBySearch} />
          
           </div>
           <div id={styles.itemList}>
-            <ol>
-              {filteredList.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ol>
+            <div className="list-group">
+              {    console.log(filteredList)}
+            {filteredList.map((item, id) => (
+              (filteredList.length > 0) ? (
+                <a href="#" key={id} className="list-group-item list-group-item-action" onClick={(e) => takeClickedCity(item.sehir, e)}>
+                  {item.sehir}
+                </a>
+              ) : ""
+            ))}
+          </div>
           </div>
         </div>
       );
--- react/components/ProgressBar/ProgressBar.jsx
+++ react/components/ProgressBar/ProgressBar.jsx
@@ -27,30 +27,32 @@ const ProgressBar = () => {
                     <div className="progress-bar" style={{width: `${progress}%`, backgroundColor: getColor() }}>{progress}%</div>
                   </div>
     <div className="col-md-7">
-    
             <h1 className='fs-1 mb-5'>Etkinlik Oluştur
             <hr className="border border-1 opacity-75"></hr>
             </h1>
-
-          
             <form action="">
             <div className="my-3">
               <label htmlFor="exampleInputEmail1" className="form-label fs-5">Başlık<i className="fs-6 text-secondary">(Gerekli)</i></label>
-
-              <input type="email" className="form-control my-3" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              <input type="email" className="form-control my-2" placeholder='Başlık girin...' id="exampleInputEmail1" aria-describedby="emailHelp" />
             </div>
             <div className="my-3">
             <label htmlFor="exampleInputEmail1" className="form-label fs-5">Etkinliğin Tarihi<i className="fs-6 text-secondary">(Gerekli)</i></label>
-                <input id="startDate" className="form-control my-3" type="datetime-local" />
+                <input id="startDate" className="form-control my-2" type="datetime-local" />
               </div>
-            </form>
-            <div className="my-3">
-            <label htmlFor="exampleInputEmail1" className="form-label fs-5">Konumu<i className="fs-6 text-secondary">(Gerekli)</i></label>
-            <FilterLocation eleman="hello" />
-              </div>
-            
 
-            <button className="btn btn-success" onClick={handleButton}>Sonraki Aşama</button>
+              <div className="my-3">
+            <label htmlFor="exampleInputEmail1" className="form-label fs-5">Konumu<i className="fs-6 text-secondary">(Gerekli)</i></label>
+               <FilterLocation />
+              </div>
+
+
+              <div className="my-4">
+              <label htmlFor="floatingTextarea2" className="form-label fs-5"><i>Etkinliğinizde neler yapılacak?</i></label>
+              <textarea className="form-control my-2" placeholder="Sabah yürüyüşünden sonra doğa gezisine gideceğiz..." id="floatingTextarea2" style={{height: "100px"}}></textarea>
+            </div>
+          
+            </form>
+            <button className="btn text-white" style={{background: "var(--first-color)"}} onClick={handleButton}>Sonraki Aşama</button>
     </div>
 
     <div className="col-md-5">
@@ -59,11 +61,6 @@ const ProgressBar = () => {
               <h2 className="fs-4 text-primary text-secondary-emphasis fw-semibold" style={{fontFamily: 'var(--second-font)'}}>
                 Etkinlik Oluşturmanın Püf Noktaları
                 </h2>
-            </div>
-        </div>
-        <div className="container mt-3 d-flex justify-content-center" style={{height: "100%"}}>
-            <div className="w-75 bg-white d-block p-3 h-100 rounded-5">
-              <h2 className="fs-2 text-left fw-semibold">Herkesin katılmak isteyeceği bir etkinlik oluşturmanın püf noktaları</h2>
             </div>
         </div>
     </div>
